a15085aa6d75b0fcd35c27463d99fd0e393f2f0d
jonayeds <jonayeeds@gmail.com>
2024-09-25 14:53:15 +0600

    added package class

---
+++ src/tourpackages/Package.java
@@ -0,0 +1,14 @@
+package tourpackages;
+
+public class Package {
+    private int packageID;
+    private String packageName;
+    private String tourDestination;
+    private double packagePrice;
+    public Package(int packageID, String packageName, String tourDestination, double packagePrice) {
+        this.packageID=packageID;
+        this.packageName=packageName;
+        this.tourDestination=tourDestination;
+        this.packagePrice=packagePrice;
+    }
+}
